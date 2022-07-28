e5640143ee09628d46a193550a5a82c916c86b42
Michael Whitehead <michael.r.whitehead@gmail.com>
2022-07-28 11:26:56 +1000

    Add files via upload

---
+++ agvic_gplot2_theme.R
@@ -0,0 +1,41 @@
+### Agriculture Victoria branding ggplot2 theme
+
+# Loads font VIC, plot headings in AgVic green, plot theme "nice". For more information on branding guidelines check intranet here: 
+# https://intranet.djpr.vic.gov.au/policies-forms-and-templates/agriculture-victoria-entity-brand-style-guide-and-templates
+
+# using this theme will require the download and installation of font family "VIC", more information here: https://www.vic.gov.au/brand-victoria-fonts
+
+
+#load dependencies
+library(extrafont)
+library(ggplot2)
+library(jtools)
+loadfonts("win")
+
+
+## change global theme settings (for all following plots)
+theme_set(theme_nice(base_size= 12, base_family= "VIC"))
+
+
+## modify plot elements globally (for all following plots)
+theme_update(
+  axis.ticks= element_line(color = "grey92"),
+  axis.ticks.length= unit(.5, "lines"),
+  panel.grid.minor= element_blank(),
+  legend.title= element_text(size = 12, color = "grey30"), 
+  legend.text= element_text(color = "grey30"),
+  plot.title= element_text(hjust = 0, size = 18, face = "bold", color = '#00573F'),
+  plot.subtitle= element_text(size = 11, color = "grey30"),
+  plot.caption= element_text(size = 9, margin = margin(t = 15)), 
+  plot.margin= margin(25, 25, 10, 25), 
+  plot.title.position= 'plot', plot.caption.position= 'plot'
+)
+
+### example plot
+box <- ggplot(data=iris, aes(x=Species, y=Sepal.Length))
+box + geom_boxplot(aes(fill=Species)) + 
+  ylab("Sepal Length") + ggtitle("Iris Boxplot") +
+  stat_summary(fun.y=mean, geom="point", shape=5, size=4) +
+  labs(    caption = "Agriculture Victoria ggplot2 theme")
+
+
